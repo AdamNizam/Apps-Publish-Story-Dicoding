cda82526122191f33c20708dcd85c00497ce50cd
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-09-29 22:48:12 +0700

    motion

--- app/src/main/java/com/example/storyapps/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/storyapps/ui/RegisterActivity.kt
@@ -1,21 +1,36 @@
 package com.example.storyapps.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.storyapps.R
+import com.example.storyapps.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.registerMain) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.loginTextView.setOnClickListener { toLoginPage() }
+    }
+
+    private fun toLoginPage(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
