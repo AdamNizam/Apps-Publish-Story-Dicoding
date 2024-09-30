8136404fd6fdc608b3f55be0f66a4f5992c6709c
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-09-30 21:13:44 +0700

    add animate

--- app/src/main/java/com/example/storyapps/ui/LoginActivity.kt
+++ app/src/main/java/com/example/storyapps/ui/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.storyapps.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,6 +28,38 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val passwordEditText = binding.passwordEditText
+        val passwordInputLayout = binding.passwordInputLayout
+        val emailEditText = binding.emailEditText
+        val emailInputLayout = binding.emailInputLayout
+
+        emailEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().contains("@gmail.com")) {
+                    emailInputLayout.error = "Email harus Di isi"
+                } else {
+                    emailInputLayout.error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) { }
+        })
+
+        passwordEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length < 8) {
+                    passwordInputLayout.error = "Password tidak boleh kurang dari 8 karakter"
+                } else {
+                    passwordInputLayout.error = null
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
 
         binding.registerTextView.setOnClickListener { toRegisterPage() }
     }
--- app/src/main/java/com/example/storyapps/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/storyapps/ui/RegisterActivity.kt
@@ -2,6 +2,8 @@ package com.example.storyapps.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -27,6 +29,40 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.loginTextView.setOnClickListener { toLoginPage() }
+        val passwordEditText = binding.passwordEditText
+        val passwordInputLayout = binding.passwordInputLayout
+        val emailEditText = binding.emailEditText
+        val emailInputLayout = binding.emailInputLayout
+
+        emailEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().contains("@gmail.com")) {
+                    emailInputLayout.error = "Email harus Di isi"
+                } else {
+                    emailInputLayout.error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) { }
+        })
+
+        passwordEditText.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().length < 8) {
+                    passwordInputLayout.error = "Password tidak boleh kurang dari 8 karakter"
+                } else {
+                    passwordInputLayout.error = null
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+
     }
 
     private fun toLoginPage(){
