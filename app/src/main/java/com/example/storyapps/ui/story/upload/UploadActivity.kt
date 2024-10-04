4cfd95d6be830cf9eb60ff1f423764c962cba745
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-10-04 22:38:34 +0700

    fitur camera

--- app/src/main/java/com/example/storyapps/ui/story/upload/UploadActivity.kt
+++ app/src/main/java/com/example/storyapps/ui/story/upload/UploadActivity.kt
@@ -11,12 +11,62 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.storyapps.databinding.ActivityUploadBinding
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.view.OrientationEventListener
+import android.view.Surface
+import androidx.camera.core.ImageCapture
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
+import com.example.storyapps.ui.story.upload.CameraActivity.Companion.CAMERAX_RESULT
 
 class UploadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUploadBinding
 
     private var currentImageUri: Uri? = null
+
+    private var imageCapture: ImageCapture? = null
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private val orientationEventListener by lazy {
+        object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation == ORIENTATION_UNKNOWN) {
+                    return
+                }
+                val rotation = when (orientation) {
+                    in 45 until 135 -> Surface.ROTATION_270
+                    in 135 until 225 -> Surface.ROTATION_180
+                    in 225 until 315 -> Surface.ROTATION_90
+                    else -> Surface.ROTATION_0
+                }
+                imageCapture?.targetRotation = rotation
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +80,11 @@ class UploadActivity : AppCompatActivity() {
             insets
         }
 
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
         binding.buttonGallery.setOnClickListener { startGallery() }
-        binding.buttonCamera.setOnClickListener { startCameraX() }
+        binding.buttonCamera.setOnClickListener { startCamera() }
 
 
     }
@@ -51,8 +104,18 @@ class UploadActivity : AppCompatActivity() {
         }
     }
 
-    private fun startCameraX() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    private fun startCamera() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERAX_RESULT) {
+            currentImageUri = it.data?.getStringExtra(CameraActivity.EXTRA_CAMERAX_IMAGE)?.toUri()
+            showImage()
+        }
     }
 
     private fun showImage() {
@@ -64,6 +127,19 @@ class UploadActivity : AppCompatActivity() {
 
     private fun uploadImage() {
         Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isPhotoPickerAvailable(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+    }
+
+    override fun onStart() {
+        super.onStart()
+        orientationEventListener.enable()
+    }
+    override fun onStop() {
+        super.onStop()
+        orientationEventListener.disable()
     }
 
 }
